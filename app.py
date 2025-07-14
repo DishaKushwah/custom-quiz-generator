496274b662e1671f092fecb60cf6228b1ffb5c50
aditizyy <105597760+aditizyy@users.noreply.github.com>
2025-07-14 22:16:32 +0530

    Update app.py -removed kashika's commit

--- app.py
+++ app.py
@@ -58,23 +58,3 @@ if st.button("📩 Submit Feedback"):
         st.info("✏️ Please write something before submitting.")
     else:
         st.success("🙌 Thank you for your feedback!")
-#=======
-import streamlit as st
-import time
-st.title("AI Custom Quiz Generator")
-st.markdown("""
-Welcome to the AI-Powered Quiz Generator!  
-Enter your preferences below to generate personalized quizzes based on your topic, difficulty level, and question type.
-""")
-topic = st.text_input("Enter Topic :")
-difficulty = st.selectbox("Select Difficulty Level:", ["Easy", "Medium", "Hard"])
-q_type = st.selectbox("Question Type:", ["MCQ", "True/False", "Short Answer"])
-num_questions = st.slider("Number of Questions:", 1, 20, 5)
-if st.button("🚀 Generate Quiz"):
-    if topic.strip() == "":
-        st.warning("Please enter a topic before generating the quiz.")
-    else:
-        with st.spinner("Generating your quiz... Please wait..."):
-            time.sleep(2) 
-        st.success("Quiz generated!")
-#>>>>>>> main
